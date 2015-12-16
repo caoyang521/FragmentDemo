fb0dadc711739827905554e3289ae571a7cd1a36
Mr_TT <981319008@qq.com>
2015-12-16 21:36:33 +0800

    AsyncTask

--- app/src/main/java/com/atg/fragment/Fragment01.java
+++ app/src/main/java/com/atg/fragment/Fragment01.java
@@ -2,10 +2,15 @@ package com.atg.fragment;
 
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.net.URL;
 
 
 /**
@@ -22,4 +27,40 @@ public class Fragment01 extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Thread(){
+            @Override
+            public void run() {
+                Looper.prepare();
+                Handler handler=new Handler();
+                Looper.loop();
+            }
+        }.start();
+    }
+
+    private class MyAsyncTask extends AsyncTask<URL,Integer,Long>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Long doInBackground(URL... params) {
+            return null;
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+        }
+    }
 }
